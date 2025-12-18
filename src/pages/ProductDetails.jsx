02b69240c1d135257c2f7aefff80ae6884788081
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-18 16:53:50 +0600

    Added Dynamic Title For Products Details Page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -68,6 +68,7 @@ const ProductDetails = () => {
 
     return (
         <div className='max-w-7xl md:mx-auto mx-3 my-10'>
+            <title>Products Details</title>
             <div className='grid grid-cols-1 md:grid-cols-2 justify-center items-start md:gap-10 gap-5 bg-slate-800 md:p-10 p-3 rounded-xl h-full'>
                 <div className='w-full h-full'>
                     <img src={product.productImage} alt={product.productName} className='rounded-xl md:w-full md:h-100 overflow-hidden' />
