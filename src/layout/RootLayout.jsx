8e8b1cbb393cb8ebb449dfa7d962ce3d1dbb96d6
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-18 17:19:46 +0600

    Added Dark and Light Theme Toggle Functionalities In RootLayout

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -3,16 +3,21 @@ import { Outlet } from 'react-router';
 import Navbar from '../components/common/Navbar';
 import Footer from '../components/common/Footer';
 import Banner from '../components/home/Banner';
+import useAuth from '../hooks/useAuth';
 
 const RootLayout = () => {
+
+    const { toggle } = useAuth();
+
+
     return (
-        
-        <div className='bg-slate-900'>
+
+        <div className={`${toggle && 'bg-slate-900'}`}>
             <Navbar />
-            
+
             <div className='min-h-screen'>
                 <Outlet />
-            </div>            
+            </div>
 
             <Footer />
         </div>
