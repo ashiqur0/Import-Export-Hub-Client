88b6478977e8dde390c72d03cc4120106e330adf
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-18 18:14:34 +0600

    Implement Dark Light Theme Toggle Functionalities In PageNotFound Page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import errorImg from '../assets/error-404.png'
 import Navbar from '../components/common/Navbar';
 import Footer from '../components/common/Footer';
+import useAuth from '../hooks/useAuth';
 
 const PageNotFound = () => {
+
+    const { toggle } = useAuth();
 
     return (
         <div>
             <title>Page Not Found</title>
             <Navbar></Navbar>
-            <div className='bg-slate-900 w-full md:mx-auto flex justify-center items-center py-20 px-10'>
+            <div className={`${toggle && 'bg-slate-900'} w-full md:mx-auto flex justify-center items-center py-20 px-10`}>
                 <img src={errorImg} alt="" />
             </div>
             <Footer></Footer>
