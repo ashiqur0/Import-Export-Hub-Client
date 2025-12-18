56285a2fff5f44dff92510bc846ff165ea52695f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-18 17:53:39 +0600

    Implement Dark Light Theme Toggle Functionalities In GlobalTraders Component

--- src/components/home/GlobalTraders.jsx
+++ src/components/home/GlobalTraders.jsx
@@ -2,6 +2,7 @@ import Marquee from "react-fast-marquee";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { use } from "react";
 import { FaArrowTrendUp } from "react-icons/fa6";
+import useAuth from "../../hooks/useAuth";
 
 const top_exporterPromise = fetch('/top_exporter.json').then(res => res.json());
 const top_importerPromise = fetch('/top_importer.json').then(res => res.json());
@@ -10,6 +11,7 @@ const GlobalTraders = () => {
 
     const exporters = use(top_exporterPromise);
     const importers = use(top_importerPromise);
+    const { toggle } = useAuth();
 
     return (
         <div className='my-10'>
@@ -18,7 +20,7 @@ const GlobalTraders = () => {
             <div className='bg-stone-800 mt-5'>
                 <div className='grid grid-cols-21 '>
                     <div className='md:col-span-4 col-span-6'>
-                        <h2 className='bg-amber-900 md:p-8 px-0 py-[41px] md:text-3xl text-xl font-bold text-center'>Top <br />Exporters</h2>
+                        <h2 className={`bg-amber-900 md:p-8 px-0 py-[41px] md:text-3xl text-xl font-bold text-center ${!toggle && 'text-white'}`}>Top <br />Exporters</h2>
                         <div className='text-green-700 text-2xl font-semibold flex items-end md:mt-15 mt-14 md:ml-34 ml-3'>Value <MdKeyboardArrowRight /></div>
                         <div className='text-orange-500 md:text-2xl text-2xl font-semibold flex items-end mt-2 border pl-3 border-stone-700 md:ml-30 mr-4 mb-4'>Share <MdKeyboardArrowRight /></div>
                     </div>
@@ -27,7 +29,7 @@ const GlobalTraders = () => {
                             <div className='grid grid-cols-10'>
                                 {
                                     exporters.map(exporter => <div key={exporter.company_name} className="ml-5">
-                                        <p className=' text-xl font-semibold my-1'>{exporter.country}</p>
+                                        <p className={`${!toggle && 'text-white'} text-xl font-semibold my-1`}>{exporter.country}</p>
                                         <img src={exporter.company_logo} alt={exporter.company_name} className="w-38 mt-5" />
                                         <div className='text-green-700 text-2xl font-semibold flex items-center mt-15 gap-2'><p>{exporter.exported_value_per_year}</p><FaArrowTrendUp /></div>
                                         <div className='text-orange-500 text-2xl font-semibold flex items-center mt-2 mb-4 gap-2'><FaArrowTrendUp /><p>{exporter.global_market_share}</p></div>
@@ -41,7 +43,7 @@ const GlobalTraders = () => {
 
                 <div className='grid grid-cols-21 mt-10'>
                     <div className='md:col-span-4 col-span-6'>
-                        <h2 className='bg-amber-900 md:p-8 px-0 py-[41px] md:text-3xl text-xl font-bold text-center'>Top <br />Importers</h2>
+                        <h2 className={`bg-amber-900 md:p-8 px-0 py-[41px] md:text-3xl text-xl font-bold text-center ${!toggle && 'text-white'}`}>Top <br />Importers</h2>
                         <div className='text-green-700 text-2xl font-semibold flex items-end md:mt-15 mt-14 md:ml-34 ml-3'>Value <MdKeyboardArrowRight /></div>
                         <div className='text-orange-500 md:text-2xl text-2xl font-semibold flex items-end mt-2 border pl-3 border-stone-700 md:ml-30 mr-4 mb-4'>Share <MdKeyboardArrowRight /></div>
                     </div>
@@ -50,7 +52,7 @@ const GlobalTraders = () => {
                             <div className='grid grid-cols-10'>
                                 {
                                     importers.map(exporter => <div key={exporter.company_name} className="ml-5">
-                                        <p className=' text-xl font-semibold my-1'>{exporter.country}</p>
+                                        <p className={`${!toggle && 'text-white'} text-xl font-semibold my-1`}>{exporter.country}</p>
                                         <img src={exporter.company_logo} alt={exporter.company_name} className="w-38 mt-5" />
                                         <div className='text-green-700 text-2xl font-semibold flex items-center mt-15 gap-2 text-start'><p>{exporter.imported_value_per_year}</p> <FaArrowTrendUp /></div>
                                         <div className='text-orange-500 text-2xl font-semibold flex items-center mt-2 mb-4 gap-2 text-start'><FaArrowTrendUp /> <p>{exporter.global_market_share}</p></div>
