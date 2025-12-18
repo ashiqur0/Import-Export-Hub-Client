787b38734d33eff1abb7550adf6ab865accfb722
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-18 18:12:45 +0600

    Implement Dark Light Theme Toggle Functionalities In AddExport Page

--- src/pages/AddExport.jsx
+++ src/pages/AddExport.jsx
@@ -2,11 +2,13 @@ import React, { use } from 'react';
 import AuthContext from '../context/AuthContext';
 import Swal from 'sweetalert2';
 import useAxiosSecure from '../hooks/useAxiosSecure';
+import useAuth from '../hooks/useAuth';
 
 const AddExport = () => {
 
     const { user } = use(AuthContext);
     const axiosSecure = useAxiosSecure();
+    const { toggle } = useAuth();
 
     const handleAddExport = (e) => {
         e.preventDefault();
@@ -28,7 +30,7 @@ const AddExport = () => {
             exporter_name: user.name,
             exporter_email: user.email
         };
-        
+
         // create a new product || only logged in user can create a new product
         axiosSecure.post(`/products/?email=${user.email}`, newProduct)
             .then(data => {
@@ -50,7 +52,7 @@ const AddExport = () => {
             <title>Add Exports</title>
             <h1 className='text-2xl font-semibold my-5'>Export a product</h1>
 
-            <div className='md:w-2/5 mx-auto bg-slate-800 md:p-6 p-2 rounded-xl'>
+            <div className={`md:w-2/5 mx-auto ${toggle && 'bg-slate-800'} md:p-6 p-2 rounded-xl border border-slate-400`}>
                 <form onSubmit={handleAddExport}
                     className='card-body'>
                     <fieldset className='fieldset'>
@@ -58,48 +60,54 @@ const AddExport = () => {
                         <label htmlFor="label">Product Name</label>
                         <input
                             type="text"
-                            className='input w-full bg-slate-800'
+                            className={`input w-full ${toggle && 'bg-slate-800'}`}
                             name='product_name'
+                            placeholder= 'Product Name'
                             required
                         />
 
                         <label htmlFor="label">Product Image</label>
                         <input
                             type="text"
-                            className='input w-full bg-slate-800'
+                            className={`input w-full ${toggle && 'bg-slate-800'}`}
                             name='product_image'
+                            placeholder= 'Product Image'
                             required
                         />
 
                         <label htmlFor="label">Product Price</label>
                         <input
                             type="text"
-                            className='input w-full bg-slate-800'
+                            className={`input w-full ${toggle && 'bg-slate-800'}`}
                             name='product_price'
+                            placeholder= 'Product Price'
                             required
                         />
 
                         <label htmlFor="label">Origin Country</label>
                         <input
                             type="text"
-                            className='input w-full bg-slate-800'
+                            className={`input w-full ${toggle && 'bg-slate-800'}`}
                             name='origin_country'
+                            placeholder= 'Origin Country'
                             required
                         />
 
                         <label htmlFor="label">Rating</label>
                         <input
                             type="text"
-                            className='input w-full bg-slate-800'
+                            className={`input w-full ${toggle && 'bg-slate-800'}`}
                             name='rating'
+                            placeholder= 'Rating'
                             required
                         />
 
                         <label htmlFor="label">Available Quantity</label>
                         <input
                             type="text"
-                            className='input w-full bg-slate-800 mb-7'
+                            className={`input w-full ${toggle && 'bg-slate-800'}`}
                             name='available_quantity'
+                            placeholder= 'Available Quantity'
                             required
                         />
 
